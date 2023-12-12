c323f463d163dc22ddc1498b87c85525fedf30cf
hp <hp@DESKTOP-LDKE30N>
2023-12-11 21:05:26 -0500

    Assignment 3

--- assignment3/processNumbers.js
+++ assignment3/processNumbers.js
@@ -2,14 +2,14 @@ const fs = require('fs').promises;
 
 async function processNumbers(inputFilePath, outputFilePath) {
   try {
-    // Step 1: Read an array of numbers from a JSON file
+
     const inputData = await fs.readFile(inputFilePath, 'utf-8');
     const numbers = JSON.parse(inputData);
 
-    // Step 2: Multiply numbers by two
+
     const multipliedNumbers = numbers.map(num => num * 2);
 
-    // Step 3: Write the multiplied numbers to a new file
+
     const outputData = JSON.stringify(multipliedNumbers);
     await fs.writeFile(outputFilePath, outputData, 'utf-8');
 
@@ -19,7 +19,6 @@ async function processNumbers(inputFilePath, outputFilePath) {
   }
 }
 
-// Example usage:
 const inputFilePath = 'samples/sample_input_numbers.json';
 const outputFilePath = 'samples/output_numbers.json';
 
